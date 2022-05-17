bf28a8398455896269c87b9a2bb85c341f15caeb
Mira <5555159m@gmail.com>
2022-05-17 14:13:37 +0300

    added 2 task

--- 02_Square/Program.cs
+++ 02_Square/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//По двум заданным числам проверять является ли одно квадратом другого
+
+Console.Write("Введите число:");
+int a = int.Parse(Console.ReadLine()??"0");
+int b = int.Parse(Console.ReadLine()??"0");
+if (a == b * b)
+{
+    Console.WriteLine($"Число {a} является квадратом {b}!");
+}
+if (b == a * a)
+{
+    Console.WriteLine($"Число {b} является квадратом {a}!");
+}
+else{
+    Console.WriteLine("Числа не являются квадратом друг друга!");
+}
