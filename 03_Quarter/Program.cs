4a6d47a87616b15c1b5619857255529f548ac1e5
Mira <5555159m@gmail.com>
2022-05-17 15:06:14 +0300

    added 3 task

--- 03_Quarter/Program.cs
+++ 03_Quarter/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Задать номер четверти, показать диапазоны для возможных координат
+
+Console.WriteLine("Введите номер четверти:");
+string quarter = Console.ReadLine()??"0";
+if (quarter == "1")
+    Console.WriteLine("Координаты x < 0, y > 0 находятся в 1 четверти!");
+else if (quarter == "2")
+    Console.WriteLine("Координаты x < 0, y > 0 находятся в 2 четверти!");
+else if (quarter == "3")
+    Console.WriteLine("Координаты x < 0, y < 0 находятся в 3 четверти!");
+else if (quarter == "4")
+    Console.WriteLine("Координаты x > 0, y < 0 находятся в 4 четверти!");
+else 
+Console.WriteLine("Нет такой четверти");
