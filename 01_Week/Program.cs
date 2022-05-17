32e6506b7b3a160b116218a9c957259c8a237e90
Mira <5555159m@gmail.com>
2022-05-17 14:11:22 +0300

    added 1 task

---
+++ 01_Week/Program.cs
@@ -0,0 +1,10 @@
+﻿//Дано число обозначающее день недели. Выяснить является номер дня недели выходным
+
+Console.Write("Ввести день недели:");
+int daynumber = int.Parse(Console.ReadLine()??"0");
+if (daynumber == 6 || daynumber == 7 )
+   Console.WriteLine("Веденный день недели является выходным днем!");
+else 
+   Console.WriteLine("Введенный день недели не является выходным днем!");
+
+
